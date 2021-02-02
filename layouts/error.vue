a20dcc7d12d2d47d8a4bc4af7e1252c1ab949d6b
Mikey Lau <mikey.lau@traininblocks.com>
2021-02-02 18:20:35 +0000

    Error page

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<style scoped>
+  #error {
+    padding: 0 8%
+  }
+</style>
+
+<template>
+  <div id="error">
+    <div class="spacer--large" />
+    <p class="text--large">Error 404</p>
+    <p class="text--large grey">Page not found</p>
+  </div>
+</template>
