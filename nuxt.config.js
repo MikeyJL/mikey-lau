65d4638ed32813fca4fda85f961ffd762dc4092d
Mikey Lau <mikey.lau@traininblocks.com>
2021-02-02 15:13:40 +0000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'mikey-lau',
+    title: 'Mikey Lau',
     htmlAttrs: {
       lang: 'en'
     },
